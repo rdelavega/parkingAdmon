79de1d756a900dcfd1dc17d39bfe789988ba561f
Ricardo de la Vega Barrón <rdelavega63@gmail.com>
2017-10-30 23:04:09 -0600

    usersSector

--- queryDB.c
+++ queryDB.c
@@ -483,7 +483,34 @@ void hourParking(MYSQL *mysql) {
         dbQuery(buffer,mysql,&res);
 
         while ((row = mysql_fetch_row(res))) {
-                printf("\t\t%s\n", row[0]);
+                printf("\t\t%s horas\n", row[0]);
+                i++;
+        }
+
+        if (i == 0) {
+                printf("\n\tNo hay registros!!\n");
+        }
+
+        printf("\n\n\n\tPresione Enter para continuar...");
+        getchar();
+}
+
+void usersSector(MYSQL *mysql) {
+        char buffer[1024];
+        MYSQL_RES *res;
+        MYSQL_ROW row;
+        int i = 0;
+
+        system("clear");
+        printf("\n\n\n\tHorarios mas concurridos por estacionamiento\n\n\n\n");
+
+        int x = selSector(mysql);
+
+        sprintf(buffer,"SELECT hour(entryof) FROM p1_park LEFT JOIN p1_parking USING(id_parking) LEFT JOIN p1_sector USING(id_sector) WHERE id_sector = %d GROUP BY hour(entryof) HAVING count(id_park) = (SELECT count(id_park) FROM p1_park LEFT JOIN p1_parking USING(id_parking) LEFT JOIN p1_sector USING(id_sector) WHERE id_sector = %d GROUP BY hour(entryof) ORDER BY count(id_park) DESC LIMIT 1);",x,x);
+        dbQuery(buffer,mysql,&res);
+
+        while ((row = mysql_fetch_row(res))) {
+                printf("\t\t%s horas\n", row[0]);
                 i++;
         }
 
